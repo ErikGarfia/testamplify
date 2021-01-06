163162d118b2da1a8396820dccc3b6ed95c36cca
ErikGarfia <erik-22-34-21@hotmail.com>
2021-01-06 08:58:23 -0600

    trying stepper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import MStepper from "materialize-stepper/dist/js/mstepper";
+declare const MStepper: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  
+  async ngOnInit() {
+    
+    var stepper = document.querySelector('.stepper');
+    var stepperInstace = new MStepper(stepper, {
+       // options
+       firstActive: 0, // this is the default
+       linearStepsNavigation: false,
+       autoFocusInput: true,
+       stepTitleNavigation: true,
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {NgStepperModule} from 'angular-ng-stepper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepOneComponent } from './components/steps/step-one/step-one.component';
+import { StepTwoComponent } from './components/steps/step-two/step-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepOneComponent,
+    StepTwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CdkStepperModule,
+    NgStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
